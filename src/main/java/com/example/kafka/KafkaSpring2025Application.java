c3a20823857cd2be36f24794cc0ce24e5eee8d9c
Vermouth-Singed <qhddusl2004@naver.com>
2025-08-27 14:14:36 +0900

    initial commit

---
+++ src/main/java/com/example/kafka/KafkaSpring2025Application.java
@@ -0,0 +1,13 @@
+package com.example.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSpring2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaSpring2025Application.class, args);
+	}
+
+}
